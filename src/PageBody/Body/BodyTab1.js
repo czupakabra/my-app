772ad8b57bfb496b834dcd58fad63162ce0bd64a
jebacPWSZ <xxtunngle@gmail.com>
2019-01-31 02:03:55 +0100

    slide icon update

--- src/PageBody/Body/BodyTab1.js
+++ src/PageBody/Body/BodyTab1.js
@@ -14,17 +14,7 @@ class BodyTab1 extends Component {
                         <img src={require("../../Image/SlideShow1/ridwan-chandra-choa-archer-002a.jpg")} alt="image2" />
                         <img src={require("../../Image/SlideShow1/ridwan-chandra-choa-archer-003a.jpg")} alt="image3" />
                     </SlideShow>
-                </section>
-
-                <section id="SlideShow" className="SlideShowContainer2">
-                    <SlideShow>
-                        <img src={require("../../Image/SlideShow2/1.jpg")} alt="image1" />
-                        <img src={require("../../Image/SlideShow2/2.jpg")} alt="image2" />
-                        <img src={require("../../Image/SlideShow2/3.jpg")} alt="image3" />
-                        <img src={require("../../Image/SlideShow2/4.jpg")} alt="image4" />
-                        <img src={require("../../Image/SlideShow2/5.jpg")} alt="image5" />
-                    </SlideShow>
-                </section>  
+                </section>                 
             </section>                         
         );
     }
