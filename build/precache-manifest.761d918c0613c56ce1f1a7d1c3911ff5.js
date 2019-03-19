39bdd4e385fdc9276656dca142a039cfbbdadec2
jebacPWSZ <xxtunngle@gmail.com>
2019-03-19 15:42:56 +0100

    Dist package added

---
+++ build/precache-manifest.761d918c0613c56ce1f1a7d1c3911ff5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "5018c7802e7a0bf6d5c32d31a44e1b12",
+    "url": "/static/media/ArrowRight.5018c780.png"
+  },
+  {
+    "revision": "d25eba05d24ac63588d2",
+    "url": "/static/css/main.69d2956b.chunk.css"
+  },
+  {
+    "revision": "b2a2c5ce0b6b1c4d1d98",
+    "url": "/static/js/1.b2a2c5ce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ef6296d27d7bab236025bbd19f75c48",
+    "url": "/static/media/Circle.5ef6296d.png"
+  },
+  {
+    "revision": "129beedbc777e53acb58714ae75361b3",
+    "url": "/static/media/CircleFill.129beedb.png"
+  },
+  {
+    "revision": "ef90017e14acf86a984d89cf24d54c6e",
+    "url": "/static/media/close.ef90017e.png"
+  },
+  {
+    "revision": "cbcffc366db08a6105e9405d38707c86",
+    "url": "/static/media/play.cbcffc36.png"
+  },
+  {
+    "revision": "40bf205e22df0357896e9449b8cdbb44",
+    "url": "/static/media/audio.40bf205e.png"
+  },
+  {
+    "revision": "d25eba05d24ac63588d2",
+    "url": "/static/js/main.d25eba05.chunk.js"
+  },
+  {
+    "revision": "7d765d975ae7a2fd7c0c0895e26a4741",
+    "url": "/static/media/ArrowLeft.7d765d97.png"
+  },
+  {
+    "revision": "5ef8494f15048b691d7c3f601bda6764",
+    "url": "/static/media/ridwan-chandra-choa-archer-001a.5ef8494f.jpg"
+  },
+  {
+    "revision": "9b3723941bb69e7865ec128b2a065bf7",
+    "url": "/static/media/ridwan-chandra-choa-archer-002a.9b372394.jpg"
+  },
+  {
+    "revision": "61608f57ba61f47e43a08e802e5ff53b",
+    "url": "/static/media/ridwan-chandra-choa-archer-003a.61608f57.jpg"
+  },
+  {
+    "revision": "7066de1a770ee1bcde3daf0d77e79227",
+    "url": "/static/media/Garbage_Not_Your_Kind_of_People.7066de1a.mp3"
+  },
+  {
+    "revision": "30714e354d2c9203ef6208a009b2c26f",
+    "url": "/static/media/Leona_Lewis_My_Hands.30714e35.mp3"
+  },
+  {
+    "revision": "add0cc5ccf5f12ef48fbe1cc29c4c0c6",
+    "url": "/static/media/RagnBone_Man_Human.add0cc5c.mp3"
+  },
+  {
+    "revision": "5f44b497ed9fdb758e4e0af0fad7ad67",
+    "url": "/index.html"
+  }
+];
