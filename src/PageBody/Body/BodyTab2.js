ad2fc3f7f4384fc7b484028d8b4b90501af23a6b
jebacPWSZ <xxtunngle@gmail.com>
2019-03-20 14:05:56 +0100

    update item selector

--- src/PageBody/Body/BodyTab2.js
+++ src/PageBody/Body/BodyTab2.js
@@ -9,7 +9,7 @@ class BodyTab2 extends Component {
         super(props);
         this.state = { audioList: null };
 
-        this.itemsArr = ['Option 1','Option 2','Option 3', 'Option 4','Option 5','Option 6'];
+        this.itemsArr = ['Option 1','Option 2','Option 3', 'Option 4','Option 5','Option 6','Option 7','Option 8','Option 9','Option 10'];
     }
 
     _selectorHendle(selectedItems){
